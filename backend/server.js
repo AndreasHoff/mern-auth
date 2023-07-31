f8feb28b42663e57cc98d366abd8d5eba77c4476
AndreasHoff <mchoffn@hotmail.com>
2023-08-01 00:20:43 +0200

    user schema creation

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,14 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import connectDB  from './config/db.js'
 const port = process.env.PORT || 5000;
 import userRoutes from './routes/userRoutes.js';
+
+connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const app = express();
 
