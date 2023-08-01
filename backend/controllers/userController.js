1d12c73148a3934e2f7431495788d0fa1eb81e90
AndreasHoff <mchoffn@hotmail.com>
2023-08-01 23:51:59 +0200

    added frontend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,9 +3,21 @@ import User from '../models/userModels.js';
 import generateToken from '../utils/generateToken.js';
 
 const authUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: 'User is authenticated'
-    });
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+        generateToken(res, user._id);
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        });
+    } else {
+        res.status(400);
+        throw new Error('Invalid email or password');
+    }
 });
 
 const registerUser = asyncHandler(async (req, res) => {
