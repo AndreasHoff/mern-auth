fb01dc04c37f5c08c25a648a4034bf3efb223d1d
AndreasHoff <mchoffn@hotmail.com>
2023-08-01 23:37:45 +0200

    backend complete

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -38,21 +38,47 @@ const registerUser = asyncHandler(async (req, res) => {
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
+    res.cookie('jwt', '', {
+        httpOnly: true,
+        expires: new Date(0)
+    });
     res.status(200).json({
-        message: 'Logout User'
+        message: 'User logged out successfully',
     });
 });
 
 const getUserProfile = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: 'User profile'
-    });
+    const user = {
+        _id: req.user._id,
+        name: req.user.name,
+        email: req.user.email
+    }
+
+    res.status(200).json(user);
 });
 
 const updateUserProfile = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: 'Update user profile'
-    });
+    const user = await User.findById(req.user._id);
+
+    if(user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+
+        if(req.body.password) {
+            user.password = req.body.password;
+        }
+
+        const updatedUser = await user.save();
+
+        res.status(200).json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email
+        });
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
 });
 
 
